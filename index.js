e15cb01cfdfbb083d8aea2a6a421256cd8589252
satish7654 <satishsingh202dd+@gmail.com>
2024-10-03 09:45:07 +0545

    second

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function attachEventListener() {
-  let count = 0;
-  document.getElementById("clickMe").addEventListener("click", function abc() {
-    console.log("Button Click", ++count);
+// function attachEventListener() {
+//   let count = 0;
+//   document.getElementById("clickMe").addEventListener("click", function abc() {
+//     console.log("Button Click", ++count);
+//   });
+// }
+// attachEventListener();
+//*****************************************************************
+// promise
+function getData(dataId, getNextData) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      // console.log("data", dataId);
+      // res("success");
+      rej("error in code");
+      if (getNextData) {
+        getNextData();
+      }
+    }, 5000);
   });
 }
-attachEventListener();
