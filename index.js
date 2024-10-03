9551ef6fee889b85b725da2a4becf13a7d574022
satish7654 <satishsingh202dd+@gmail.com>
2024-10-03 10:09:38 +0545

    third

--- index.js
+++ index.js
@@ -7,15 +7,31 @@
 // attachEventListener();
 //*****************************************************************
 // promise
-function getData(dataId, getNextData) {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      // console.log("data", dataId);
-      // res("success");
-      rej("error in code");
-      if (getNextData) {
-        getNextData();
-      }
-    }, 5000);
+// function getData(dataId, getNextData) {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       // console.log("data", dataId);
+//       // res("success");
+//       rej("error in code");
+//       if (getNextData) {
+//         getNextData();
+//       }
+//     }, 5000);
+//   });
+// }
+/************************************************************************* */
+
+const getPromise = () => {
+  return new Promise((resolve, reject) => {
+    console.log("I am promise");
+    // resolve("sucsess");
+    reject("Network Error");
   });
-}
+};
+let promise = getPromise();
+promise.then((res) => {
+  console.log("promise filled", res);
+});
+promise.catch((err) => {
+  console.log("Promise reject", err);
+});
